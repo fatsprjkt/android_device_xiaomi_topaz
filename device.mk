f5164444a92183a72d1fdda878ee9dbdace2d869
hefatss <fatsurbaby@gmail.com>
2024-02-18 13:22:10 +0000

    topaz: rro_overlays: re-Import CarrierConfig from PA * from https://github.com/XIAOMI-SM6225-bengal-5-15/device_xiaomi_topaz/tree/topaz/overlay/TopazCarrierConfigOverlay

--- device.mk
+++ device.mk
@@ -376,7 +376,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
-    CarrierConfigResCommon \
+    CarrierConfigResTopaz \
     FrameworksResCommon \
     FrameworksResTarget \
     FrameworksResTopaz \
