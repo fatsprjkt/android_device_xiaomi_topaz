84e7228652300d874a14165f13cefc8eee0a2066
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-05-01 16:17:44 +0300

    topaz: Build missing android.hidl.base@1.0 from source
    dlopen failed: library "android.hidl.base@1.0.so" not found: needed by /system_ext/lib64/libwfdnative.so in namespace clns-shared-4

--- device.mk
+++ device.mk
@@ -252,6 +252,7 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
     android.hidl.allocator@1.0.vendor \
     android.hidl.base@1.0.vendor \
     libhidltransport.vendor \
