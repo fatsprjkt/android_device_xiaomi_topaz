c6c85099cdc223e5f1b07a3528e9dd96a22183fa
hefatss <ashleyy99@proton.me>
2024-05-26 02:19:46 +0000

    topaz: Initialize for Evolution X 9.x

--- lineage_topaz.mk
+++ lineage_topaz.mk
@@ -14,10 +14,28 @@ $(call inherit-product, device/xiaomi/topaz/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+WITH_GMS := true
+
+#EvolutionX prop
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_USES_MINI_GAPPS := true
+
+# Device props
+TARGET_SUPPORTS_BLUR := false
+TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_DISABLE_EPPE := true
+TARGET_DEBLOAT := true
+
+# Charging Animation
+USE_PIXEL_CHARGER := true
+
 PRODUCT_NAME := lineage_topaz
 PRODUCT_DEVICE := topaz
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 12 4G
+
+BUILD_FINGERPRINT := Redmi/topaz_global/topaz:13/TKQ1.221114.001/V816.0.2.0.UMGMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
